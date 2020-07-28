02ed63c195875150e20673a4f94f894ab6628c8f
teenari <teenari@users.noreply.github.com>
2020-07-27 17:00:02 -0700

    Update 6a2f41a3.js

--- dashboard/src/js/6a2f41a3.js
+++ dashboard/src/js/6a2f41a3.js
@@ -319,7 +319,7 @@ async function createImageInElement(element, hidden, argumen, callback) {
 
 function changeItem(id, cosmeticType) {
     if(cosmeticType.toLowerCase() === 'banner') return;
-    fetch(`http://localhost:5000/item?array=["${id}"]&function=set${cosmeticType.toLowerCase().charAt(0).toUpperCase() + cosmeticType.toLowerCase().slice(1)}`, {credentials: 'include', headers: {'Access-Control-Allow-Origin': "https://teenari.github.io"}});
+    fetch(`https://fortnitebtapi.herokuapp.com/api/account/party/item?array=["${id}"]&function=set${cosmeticType.toLowerCase().charAt(0).toUpperCase() + cosmeticType.toLowerCase().slice(1)}`, {credentials: 'include', headers: {'Access-Control-Allow-Origin': "https://teenari.github.io"}});
 }
 
 function addVariant(array, cosmeticType) {
