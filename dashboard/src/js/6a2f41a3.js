48e41ca408d5b9e02886832b7ebe4232cce01cec
teenari <teenari@users.noreply.github.com>
2020-07-27 18:49:20 -0700

    Update 6a2f41a3.js

--- dashboard/src/js/6a2f41a3.js
+++ dashboard/src/js/6a2f41a3.js
@@ -403,7 +403,13 @@ $(document).ready(async () => {
         console.log(error)
         return setLoadingText('ok');
     }
-    const source = new EventSource(`https://fortnitebtapi.herokuapp.com/api/account/session/start?auth=${(await (await fetch('https://fortnitebtapi.herokuapp.com/api/auth', {credentials: 'include', headers: {'Access-Control-Allow-Origin': "https://teenari.github.io"}})).json()).auth}`);
+    let source;
+    try {
+        source = new EventSource(`https://fortnitebtapi.herokuapp.com/api/account/session/start?auth=${(await (await fetch('https://fortnitebtapi.herokuapp.com/api/auth', {credentials: 'include', headers: {'Access-Control-Allow-Origin': "https://teenari.github.io"}})).json()).auth}`);
+    } catch(error) {
+        console.log(error)
+        return setLoadingText('ok');
+    }
 
     await new Promise((resolve) => {
         source.onmessage = (data) => {
