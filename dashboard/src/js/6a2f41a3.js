15c574eaa06270b6e2da54981202b7bf4f104344
teenari <teenari@users.noreply.github.com>
2020-07-30 13:03:59 -0700

    e

--- dashboard/src/js/6a2f41a3.js
+++ dashboard/src/js/6a2f41a3.js
@@ -12,10 +12,15 @@
  */
 
  /**
-  * <div id="members"><div style="
-    position: relative;
-    display: inline-block;
-"><img width="100" height="100" draggable="false" src="./src/images/schemes/black/back.png" style="cursor: auto;position: relative;left: 1px;"><img width="100" height="100" draggable="false" src="https://fortnite-api.com/images/cosmetics/br/CID_319_Athena_Commando_F_Nautilus/icon.png" style="cursor: auto;position: absolute;left: -3px;"><img width="100" height="100" draggable="false" src="./src/images/schemes/black/faceplate.png" style="left: 1px;cursor: auto;position: absolute;"></div></div>
+  * member
+  * first div members
+  * add member div class
+  * back png is left 1px relative
+  * image fnapi -3px absolute
+  * faceplate 1px absolute
+  * add to #text div
+  * now it'll be sorted
+  * <div id="members"><div class="member"><img width="100" height="100" draggable="false" src="./src/images/schemes/black/back.png" style="cursor: auto;position: relative;left: 1px;"><img width="100" height="100" draggable="false" src="https://fortnite-api.com/images/cosmetics/br/CID_319_Athena_Commando_F_Nautilus/icon.png" style="cursor: auto;position: absolute;left: -3px;"><img width="100" height="100" draggable="false" src="./src/images/schemes/black/faceplate.png" style="left: 1px;cursor: auto;position: absolute;"></div></div>
   */
 
 let LoadingText = '';
