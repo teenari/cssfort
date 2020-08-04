c52bcf908d4e8fd22f592b35c90982a038f9386a
teenari <teenari@users.noreply.github.com>
2020-08-03 17:41:05 -0700

    Update 6a2f41a3.js

--- dashboard/src/js/6a2f41a3.js
+++ dashboard/src/js/6a2f41a3.js
@@ -403,8 +403,20 @@ function setMembers() {
     for (const member of members) {
         const images = getImages(member.meta['Default:AthenaCosmeticLoadout_j'].AthenaCosmeticLoadout);
         $('#members')[0].innerHTML += `<div id="${member.id}" class="member"><img width="120" height="120" draggable="false" src="${settings.colorScheme[settings.currentScheme].back}"><img width="120" height="120" draggable="false" src="${images.character}"><img width="120" height="120" draggable="false" src="${settings.colorScheme[settings.currentScheme].faceplate}"><div>${member.displayName}</div></div>`;
-        $(`#${data.id}.member`).click(() => {
-            $('#menu').html(`<div class="cosmetic">${data.displayName}<br><div style="font-size: 20px; margin: 10px;"><div id="kickPlayer" class="clickHereButton">Kick Player</div></div></div>`);
+        $(`#${member.id}.member`).click(() => {
+            $(document).unbind('click');
+            $('#menu').html(`<div class="cosmetic">${member.displayName}<br><div style="font-size: 20px; margin: 10px;"><div id="kickPlayer" class="clickHereButton">Kick Player</div></div></div>`);
+            menu.fadeIn(250);
+            menu.draggable({
+                "containment": "window"
+            });
+            await new Promise((resolve) => setTimeout(resolve, 300));
+            $(document).click(async (e) => { 
+                if(!$(event.target).closest('#menu').length && $('#menu').is(":visible")) {
+                    await hideMenu();
+                    $(document).unbind('click');
+                }        
+            });
         });
     }
 }
