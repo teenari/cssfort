f4f5617198888f2233ad2003430d4802333491d5
teenari <teenari@users.noreply.github.com>
2020-08-03 17:44:00 -0700

    Update 6a2f41a3.js

--- dashboard/src/js/6a2f41a3.js
+++ dashboard/src/js/6a2f41a3.js
@@ -405,7 +405,8 @@ function setMembers() {
         $('#members')[0].innerHTML += `<div id="${member.id}" class="member"><img width="120" height="120" draggable="false" src="${settings.colorScheme[settings.currentScheme].back}"><img width="120" height="120" draggable="false" src="${images.character}"><img width="120" height="120" draggable="false" src="${settings.colorScheme[settings.currentScheme].faceplate}"><div>${member.displayName}</div></div>`;
         $(`#${member.id}.member`).click(async () => {
             $(document).unbind('click');
-            $('#menu').html(`<div class="cosmetic">${member.displayName}<br><div style="font-size: 20px; margin: 10px;"><div id="kickPlayer" class="clickHereButton">Kick Player</div></div></div>`);
+            const menu = $('#menu');
+            menu.html(`<div class="cosmetic">${member.displayName}<br><div style="font-size: 20px; margin: 10px;"><div id="kickPlayer" class="clickHereButton">Kick Player</div></div></div>`);
             menu.fadeIn(250);
             menu.draggable({
                 "containment": "window"
