20d8f26881317daa084805b925c323803c192662
teenari <teenari@users.noreply.github.com>
2020-08-11 11:06:22 -0700

    e

--- dashboard/src/js/6a2f41a3.js
+++ dashboard/src/js/6a2f41a3.js
@@ -777,6 +777,9 @@ $(document).ready(async () => {
     $('#fortnite').fadeOut(300);
     $('.menu-container').css('left', '300vh').show().animate({left: '58.5px'}, 700);
     $('#avatar').css('position', 'absolute').css('left', '-500px').show().animate({left: 10}, 700);
+    $('#ad').css('bottom', '-539px').show().animate({
+        bottom: '12px'
+    }, 1000);
     $('.members-container').fadeIn();
     stopText();
     await new Promise((resolve) => setTimeout(resolve, 300));
