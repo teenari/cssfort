561a4878277e375c8a227b15f8ce840b32bc58d7
teenari <teenari@users.noreply.github.com>
2020-08-15 22:03:06 -0700

    Update 6a2f41a3.js

--- dashboard/src/js/6a2f41a3.js
+++ dashboard/src/js/6a2f41a3.js
@@ -682,7 +682,7 @@ function setMembers() {
             let items = '';
             for (const key of Object.keys(images)) {
                 const value = images[key];
-                items += `<div class="icon" style="width: 100px;"><img width="100" height="100" draggable="false" src="${system.settings.colorScheme[system.settings.currentScheme].back}"><img width="100" height="100" draggable="false" src="${value}"><img width="100" height="100" draggable="false" src="${system.settings.colorScheme[system.settings.currentScheme].faceplate}"></div>`
+                items += `<div class="icon" style="width: 100px; height: 99px;"><img width="100" height="100" draggable="false" src="${value}"></div>`
             }
             menu.html(`<div class="cosmetic">${member.displayName}<br><div style="font-size: 20px; margin: 10px;"><div style="position: relative;align-content: end;align-items: self-end;height: 108px;display: flex;">${items}</div><div id="kickPlayer" class="clickHereButton"${member.id === system.account.id ? ' style="border: 1px solid gray;color: gray;"' : ''}>Kick Player</div></div><div style="margin: 10px;font-size: 20px;">JOINED AT: ${member.joinedAt}</div><div style="margin: 10px;font-size: 20px;">ID: ${member.id}</div><div style="margin: 10px;font-size: 20px;">ROLE: ${member.role}</div></div>`);
             menu.fadeIn(250);
