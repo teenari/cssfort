4a1ae5f96c02bf45c6ba5c31f0dceccbd2dc77cd
teenari <teenari@users.noreply.github.com>
2020-08-25 14:47:40 -0700

    Update 6a2f41a3.js

--- dashboard/src/js/6a2f41a3.js
+++ dashboard/src/js/6a2f41a3.js
@@ -124,10 +124,10 @@ class Menu {
                     $('#cosmetics')[0].appendChild(div);
                 }
             }
-            if(system.items.variants[cosmeticType]) for (const variant of system.items.variants[cosmeticType]) {
-                $(`[id="VARIANT/${variant.tag}#${variant.name}"]`).children().eq(0).animate({borderRadius: 3}, 200)
-                selectedVariants.push(variant);
-            }
+            // if(system.items.variants[cosmeticType]) for (const variant of system.items.variants[cosmeticType]) {
+            //     $(`[id="VARIANT/${variant.tag}#${variant.name}"]`).children().eq(0).animate({borderRadius: 3}, 200)
+            //     selectedVariants.push(variant);
+            // }
             $('#SaveVariant').click(async () => {
                 if(selectedVariants.length === 0) return;
                 if(!system.items.variants[cosmeticType]) system.items.variants[cosmeticType] = [];
@@ -1486,7 +1486,7 @@ $(document).ready(async () => {
     for (const account of accounts.accounts) {
         const div = document.createElement('div');
         document.getElementById('options').appendChild(div);
-        div.outerHTML = `<div id="${account}" style="overflow: hidden;width: 135px;background: #f3af19;border: 1px solid black;height: 204px;border-radius: 11px;margin: 2px;cursor: pointer;"><div style="background: #f3af19;border: 1px solid black;border-radius: 400px;width: 100px;height: 100px;overflow: hidden;position: relative;left: 17px;top: 10px;"><img src="https://fortnite-api.com/images/cosmetics/br/${cids[cids.length * Math.random() | 0]}/icon.png" style="width: 100px;"></div><div style="font-size: 42px;position: relative;top: 28px;">${account}</div></div>`;
+        div.outerHTML = `<div id="${account}" style="overflow: hidden;width: 135px;background: #f3af19;border: 1px solid black;height: 204px;border-radius: 11px;margin: 2px;cursor: pointer;"><div style="background: #f3af19;border: 1px solid white;border-radius: 400px;width: 100px;height: 100px;overflow: hidden;position: relative;left: 17px;top: 10px;"><img src="https://fortnite-api.com/images/cosmetics/br/${cids[cids.length * Math.random() | 0]}/icon.png" style="width: 100px;"></div><div style="font-size: 42px;position: relative;top: 28px;">${account}</div></div>`;
         $(`#${account}`).hover(
             () => {
                 $(`#${account}`).animate({borderRadius: 3}, 100);
