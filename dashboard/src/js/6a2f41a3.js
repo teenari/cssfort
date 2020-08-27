409d23f4467a8e3013876b7dd2ef5613a07a65ae
teenari <teenari@users.noreply.github.com>
2020-08-27 10:03:44 -0700

    oiji

--- dashboard/src/js/6a2f41a3.js
+++ dashboard/src/js/6a2f41a3.js
@@ -963,6 +963,13 @@ async function friendsMenu(menu) {
 }
 
 $(document).ready(async () => {
+    if(navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
+        const link = document.createElement('link');  
+        link.rel = 'stylesheet';  
+        link.type = 'text/css'; 
+        link.href = './src/css/mobile.css';  
+        document.getElementsByTagName('HEAD')[0].appendChild(link); 
+    }
     const user = await (await fetch(`http://webfort.herokuapp.com/api/user`, {
         credentials: 'include',
         headers: {
