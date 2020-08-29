2fc98091d7063207d778a6c6527b8e589d640c53
teenari <teenari@users.noreply.github.com>
2020-08-29 14:47:46 -0700

    Update 18a6aea8.js

--- src/js/18a6aea8.js
+++ src/js/18a6aea8.js
@@ -28,6 +28,7 @@ $(document).ready(async () => {
     if(user.error) {
         $('[id="error-content"]')[0].innerHTML = `<div>${user.error}</div><div>${user.message}</div>`;
         $('[class="error-container"]').fadeIn();
+        $('#fortnite').fadeOut();
         return;
     }
     $('#fortnite')[0].innerHTML = '<div id="items"><div id="avatar"><div id="icon"><img src="https://cdn.discordapp.com/avatars/734848834986967096/1de4e2d6318008d40ca5d8f1549ac6b5.png?size=128" width="200px"></div><div style="justify-content: center;align-items: center;width: 100%;height: 100%;"><div id="loginwithDiscord" style="background-color: rgb(32, 34, 37);color: rgb(255, 255, 255);justify-content: center;position: inherit;align-items: center;" onclick="window.location = `https://discord.com/api/oauth2/authorize?client_id=735921855340347412&amp;redirect_uri=https%3A%2F%2Fwebfort.herokuapp.com%2Fapi%2Fauthorize&amp;response_type=code&amp;scope=identify`" class="loginDiscord">Authorize</div></div></div></div>';
