69f88cf1109019a81825fa7df842c18106e1d604
teenari <teenari@users.noreply.github.com>
2020-09-15 21:26:18 -0700

    kpop

---
+++ src/js/89c05b3f.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2020 Teenari
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+$(document).ready(async () => {
+    const user = await (await fetch('https://webfort.herokuapp.com/api/user', {
+        credentials: 'include',
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        }
+    })).json();
+    await new Promise((resolve) => setTimeout(resolve, 1500));
+    // await new Promise((resolve) => $('#Understand').click(resolve));
+    
+    if(user.authorization !== false) {
+    }
+});
